515370f7608b3241ebdc7092b6ebc474d29d8d55
yodahost <64697807+yodahost@users.noreply.github.com>
2020-05-09 20:16:33 +0300

    Amdin

--- Optimizeli Başlatma/BaslatYoda.bat
+++ Optimizeli Başlatma/BaslatYoda.bat
@@ -1,4 +1,4 @@
-@ECHO OFF
+﻿@ECHO OFF
 title YODA
 color 6f
 :server_start
@@ -8,7 +8,7 @@ echo Sunucuyu baslatma. Lütfen sunucunun
 echo baslamasi icin biraz zaman taniyin.
 echo ====================================
 echo.
-start /B /high /wait java -Xmx4G -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=45 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar spigot.jar -o false
+start /B /high /wait java -Xmx4G -Dfile.encoding=UTF-8 -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=45 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar spigot.jar -o false
 echo.
 echo ====================================
 echo Bu baslangici kapatmak icin CTRL + C tuslarina basin.
--- Optimizeli Başlatma/BaslatYoda.sh
+++ Optimizeli Başlatma/BaslatYoda.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 while true
 do
-java -Xmx2G -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=45 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar Factions.jar -o false
+java -Xmx2G -XX:+AlwaysPreTouch -Dfile.encoding=UTF-8 -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=45 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar Factions.jar -o false
 echo "Bu baslangici kapatmak icin CTRL + C tuslarina basin!"
 echo "Yeniden Baslatiliyor:"
 for i in 5 4 3 2 1
